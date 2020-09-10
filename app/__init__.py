0c07a5fc9e5d36db0b3cb8cfa6aa4ee3183eafd2
Timothy Munene <munenegicharu@gmail.com>
2020-09-10 22:47:04 +0300

    deployment to heroku

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from .config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+    
+    return app
+
+
+
+
+# from app import views
+# from app import error
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='f925dbd1886dfd2d9f57a72e3b9d6f7d'
+export SECRET_KEY='<Flask WTF Secret Key>'
+
+python3.7 manage.py server
